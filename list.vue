8180ad41533dacf352088b71f1a8de94d2f74042
person-0 <13213891450@163.com>
2018-05-28 13:44:33 +0800

    add list

---
+++ list.vue
@@ -0,0 +1,34 @@
+<style lang="sass" scoped>
+.content_list
+    .content_row
+      display: flex
+</style>
+
+<template>
+    <view class="content_list">
+        <repeat for="{{list}}" item="item">
+            <view class="content_row">
+                <repeat for="{{item}}">
+                    <slot name="item"></slot>
+                </repeat>
+            </view>
+        </repeat>
+    </view>
+</template>
+
+<script>
+  import wepy from "wepy";
+  
+  export default class List extends wepy.component {
+    props = {
+      list: {
+        type: Array,
+      },
+    };
+    
+    computed = {
+      /* getList(){ return this.$root.data.faceList; } */
+    };
+    
+  }
+</script>
