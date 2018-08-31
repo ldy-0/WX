433fca5546b02fd5502ac20635c234aded5b40d5
person-0 <13213891450@163.com>
2018-08-31 20:29:47 +0800

    modify address, home banner

---
+++ src/utils/api.js
@@ -0,0 +1,188 @@
+import request from './request.js';
+
+const token = wx.getStorageSync('token');
+/**
+ * 
+ * @param {String} code 
+ */
+async function getToken(code){
+
+  let res = await request.post('/api/v2/member/login', { code });
+
+  if(res.error !== ''){
+    return wx.showModal( {title: '提示', content: res.error, showCancel: false, } );
+  } 
+
+  wx.setStorageSync('token', res.data.token);
+  return res.data.token;
+}
+
+/**
+ * 
+ * @param {Object} params 
+ */
+async function setUserInfo(params){
+
+  let res = await request.post('/api/v2/member/memberinfo', params, {
+      'token': wx.getStorageSync('token'),
+  });
+
+  if(res.error !== ''){
+    return wx.showModal( {title: '提示', content: res.error, showCancel: false, } );
+  }
+
+  return res.data;
+
+}
+
+async function setPhone(params){
+
+  let res = await request.post('/api/v2/member/telephone', params, {
+      'token': wx.getStorageSync('token'),
+  });
+
+  if(res.error !== ''){
+    return wx.showModal( {title: '提示', content: res.error, showCancel: false, } );
+  }
+
+  return res.data.telephone;
+
+}
+
+// Address
+// 省市区数据
+async function getAddressInfo(params){
+  
+  let res =await request.get('/api/v2/admin/area', params);
+
+  if(res.error !== ''){
+    return wx.showModal( {title: '提示', content: res.error, showCancel: false, } );
+  }
+
+  return res.data;
+  
+}
+
+async function setAddress(params){
+
+  let res = await request.post('/api/v2/member/address', params, {
+      'token': wx.getStorageSync('token'),
+  });
+
+  if(res.error !== ''){
+    return wx.showModal( {title: '提示', content: res.error, showCancel: false, } );
+  }
+
+  return res.data;
+}
+// 
+/**
+ * 
+ * 获取地址列表或默认地址
+ * @param {Object} params 
+ * page, limit, address_is_default
+ */
+async function getAddressList(params){
+
+  let res = await request.get('/api/v2/member/address', params, {
+      'token': wx.getStorageSync('token'),
+  });
+
+  if(res.error !== ''){
+    return wx.showModal( {title: '提示', content: res.error, showCancel: false, } );
+  }
+
+  return res.data;
+}
+
+async function updateAddress(id, params){
+
+  let res = await request.put('/api/v2/member/address/' + id, params, {
+      'token': wx.getStorageSync('token'),
+  });
+
+  if(res.error !== ''){
+    return wx.showModal( {title: '提示', content: res.error, showCancel: false, } );
+  }
+
+  return res.data;
+}
+
+async function deleteAddress(id){
+  let res = await request.delete('/api/v2/member/address/' + id, null, {
+      'token': wx.getStorageSync('token'),
+  });
+
+  if(res.error !== ''){
+    return wx.showModal( {title: '提示', content: res.error, showCancel: false, } );
+  }
+
+  return res.data;
+}
+
+// home
+async function getBanner(){
+  let res = await request.get('/api/v2/member/banner', null, {
+      'token': wx.getStorageSync('token'),
+  });
+
+  if(res.error !== ''){
+    return wx.showModal({ title: '提示', content: res.error, showCancel: false, });
+  }
+
+  return res.data;
+
+}
+
+async function getSeckill(params){
+  let res = await request.get('/api/v2/member/seckill', params, {
+      'token': wx.getStorageSync('token'),
+  });
+
+  return res.error !== '' ? wx.showModal({ title: '提示', content: res.error, showCancel: false, }) : res.data;
+}
+
+// food
+async function getStoreClass(params){
+
+  let res = await request.get('/api/v2/member/storeclass', params, {
+      'token': wx.getStorageSync('token'),
+  });
+
+  return res.error !== '' ? wx.showModal({ title: '提示', content: res.error, showCancel: false, }) : res.data;
+}
+
+async function getStoreList(params){
+
+  let res = await request.get('/api/v2/member/store', params, {
+      'token': wx.getStorageSync('token'),
+  });
+
+  return res.error !== '' ? wx.showModal({ title: '提示', content: res.error, showCancel: false, }) : res.data;
+
+}
+
+async function getGoods(params){
+
+  let res = await request.get('/api/v1/member/goodscommon', params, {
+      'token': wx.getStorageSync('token'),
+  });
+
+  return res.error !== '' ? wx.showModal({ title: '提示', content: res.error, showCancel: false, }) : res.data;
+}
+
+export default {
+  getToken,
+  setUserInfo,
+  setPhone,
+  getAddressInfo, // address
+  setAddress,
+  getAddressList,
+  updateAddress,
+  deleteAddress,
+  getBanner, // home
+  getSeckill,
+  getStoreList, // food
+  getGoods,
+  getStoreClass,
+}
--- src/utils/request.js
+++ src/utils/request.js
@@ -1,13 +1,16 @@
 import wepy from 'wepy';
 
-// const DOMAIN = 'http://192.168.31.9:9527/';
 // const DOMAIN = 'http://106.14.145.66:3000/';
-const DOMAIN = 'http://101.37.30.205/';
+// const DOMAIN = 'http://101.37.30.205/';
+const DOMAIN = 'http://121.42.184.125:88';
 // const DOMAIN = 'https://www.hi-zhan.com/';
 
 
-async function get(url, params, contentType){
-  let query = '';
+async function get(url, params, header, responseType){
+  let query = '',
+      headerConfig = {
+    'content-Type': 'application/x-www-form-urlencoded',
+  };
 
   if(!/^http/.test(url)){
 
@@ -23,17 +26,11 @@ async function get(url, params, contentType){
     console.log(query);
   }
 
-  const res = await wepy.request(url + query);
+  if(header){
 
-  return res.data;
-
-}
-
-async function post(url, params, contentType, responseType){
-
-  if(!/^http/.test(url)){
-
-    url = DOMAIN + url;
+    for(let key in header){
+      headerConfig[key] = header[key];
+    }
 
   }
 
@@ -41,9 +38,87 @@ async function post(url, params, contentType, responseType){
 
     wx.request({
       url: url,
-      header: {
-        'content-Type': contentType ? contentType : 'application/x-www-form-urlencoded',
-      },
+      header: headerConfig,
+      data: params,
+      method: 'GET',
+      responseType: responseType ? responseType : 'text',
+      success: res => resolve(res.data),
+      fail: err => wx.showToast({ title: JSON.stringify(err), icon: 'none', }),
+    });
+
+  });
+
+}
+
+async function put(url, params, header, responseType){
+
+  let headerConfig = {
+    'content-Type': 'application/x-www-form-urlencoded',
+  };
+
+  if(!/^http/.test(url)){
+
+    url = DOMAIN + url;
+
+  }
+
+  if(header){
+
+    for(let key in header){
+      headerConfig[key] = header[key];
+    }
+
+  }
+  
+
+  return new Promise(function(resolve, reject){
+
+    wx.request({
+      url: url,
+      header: headerConfig,
+      // {
+      //   'content-Type': contentType ? contentType : 'application/x-www-form-urlencoded',
+      // },
+      data: params,
+      method: 'PUT',
+      responseType: responseType ? responseType : 'text',
+      success: res => resolve(res.data),
+      fail: err => wx.showToast({ title: JSON.stringify(err), icon: 'none', }),
+    });
+
+  });
+
+}
+
+async function post(url, params, header, responseType){
+
+  let headerConfig = {
+    'content-Type': 'application/x-www-form-urlencoded',
+  };
+
+  if(!/^http/.test(url)){
+
+    url = DOMAIN + url;
+
+  }
+
+  if(header){
+
+    for(let key in header){
+      headerConfig[key] = header[key];
+    }
+
+  }
+  
+
+  return new Promise(function(resolve, reject){
+
+    wx.request({
+      url: url,
+      header: headerConfig,
+      // {
+      //   'content-Type': contentType ? contentType : 'application/x-www-form-urlencoded',
+      // },
       data: params,
       method: 'POST',
       responseType: responseType ? responseType : 'text',
@@ -55,8 +130,60 @@ async function post(url, params, contentType, responseType){
 
 }
 
+async function del(url, params, header, responseType){
+
+  let headerConfig = {
+    'content-Type': 'application/x-www-form-urlencoded',
+  };
+
+  if(!/^http/.test(url)){
+
+    url = DOMAIN + url;
+
+  }
+
+  if(header){
+
+    for(let key in header){
+      headerConfig[key] = header[key];
+    }
+
+  }
+  
+
+  return new Promise(function(resolve, reject){
+
+    wx.request({
+      url: url,
+      header: headerConfig,
+      // {
+      //   'content-Type': contentType ? contentType : 'application/x-www-form-urlencoded',
+      // },
+      data: params,
+      method: 'DELETE',
+      responseType: responseType ? responseType : 'text',
+      success: res => resolve(res.data),
+      fail: err => wx.showToast({ title: JSON.stringify(err), icon: 'none', }),
+    });
+
+  });
+
+}
+
+function toQueryString(paramObj){
+  let query = '';
+
+  for(let key in paramObj){
+    query += (query === '' ? '?' : '&') + key + '=' + paramObj[key];
+  }
+
+  return query;
+}
+
 module.exports = {
   get,
   post,
-  DOMAIN: 'http://101.37.30.205:3000',// 'https://www.hi-zhan.com', // // DOMAIN.substr(0, this.DOMAIN.length - 1) + ':3000',
+  put,
+  delete: del,
+  DOMAIN: '121.42.184.125:88'// 'http://101.37.30.205:3000',// 'https://www.hi-zhan.com', 
 };
