6dffaefa78d226f103cbe645b11d3976c7c4689b
person-0 <13213891450@163.com>
2018-10-11 20:32:17 +0800

    add template authorize page

--- mp/README.md
+++ mp/README.md
@@ -6,19 +6,19 @@
 ## 组件 / component
   
   + 轮播图 / slide
-    ### 参数 
+    ### 属性
       - config : Object  
       
         #### 配置属性 
       
           |属性名|类型|默认|描述|
           |------|-----|------|-------|
+          | `data` | Array | - | 数据(数组中每项为一个对象，对象必须有img属性) |
           | `height` | Number | 476 | 高度 |
           | `indicatorDots` | Boolean | true | 是否显示面板指示点 |
           | `indicatorActiveColor` | String | '#fff' | 当前选中的指示点颜色 |
-          | `data` | Array | - | 数据(数组中每项为一个对象，对象必须有img属性) |
 
-      - click : [Function] 
+      - go : [Function] 
         > 点击回调函数
 
 
---
+++ mp/template/authorization/index.vue
@@ -0,0 +1,160 @@
+<template>
+  <div class="container">
+
+    <topBar :config='config'></topBar>
+
+    <div class='authorization s-bg-1'>
+
+      <div class='company_info'>
+        <img class='logo' src='../../images/authorization/authorize_logo.png' alt='no found' />
+        <div class='desc s-fc-2'>Welcome !</div>
+      </div>
+
+      <button class='authorization_btn s-bg-2' open-type="getUserInfo" plain='true' lang="zh_CN" @getuserinfo="getUserInfo">
+        <img src='../../images/authorization/authorize_icon.png' alt='no found' class='authorization_icon' />
+        <div class='authorization_desc s-fc-1'>微信登入</div>
+      </button>
+
+    </div>
+
+  </div>
+</template>
+
+<script>
+import topBar from '@/components/topBar'
+import api from '@/utils/api.js'
+
+export default {
+  data () {
+    return {
+      config: {
+        title: '授权',
+        color: '#fff',
+        bg: '#937d8a'
+      },
+      userInfo: {},
+      referer: null
+    }
+  },
+
+  components: {
+    topBar
+  },
+
+  methods: {
+    async getUserInfo (e) {
+
+      if (e.mp.detail.errMsg === 'getUserInfo:ok') {
+        wx.showLoading({ title: 'Loading...' })
+
+        wx.setStorageSync('userInfo', e.mp.detail.userInfo)
+
+        let user = await api.setUserInfo({
+          wx_name: wx.getStorageSync('userInfo').nickName,
+          wx_avatar: wx.getStorageSync('userInfo').avatarUrl
+        })
+        console.log('setUser', user)
+
+        if (!user) {
+          let userInfo = await api.getUserInfo()
+          userInfo && wx.setStorageSync('userInfo', userInfo)
+
+          if (userInfo && !userInfo.phone) {
+            let res = await this.login()
+            wx.setStorageSync('code', res.code)
+            console.log('phone code--', res.code)
+          }
+
+          wx.reLaunch({ url: this.referer })
+        }
+
+        wx.hideLoading()
+      }
+    },
+    login () {
+      return new Promise(function (resolve, reject) {
+        wx.login({
+          success (res) { resolve(res) },
+          fail (e) { resolve(e) }
+        })
+      })
+    },
+    async getToken () {
+      let res = await this.login()
+      wx.setStorageSync('code', res.code)
+
+      let data = await api.getToken(res.code)
+      console.log('-- get Token --', data)
+    }
+  },
+
+  created () {
+    // this.getToken()
+  },
+
+  onLoad (params) {
+    console.log('authorization onload', params)
+    this.referer = params.referer
+  }
+
+}
+</script>
+
+<style scoped>
+
+.authorization{
+  position: absolute;
+  top: 0;
+  width: 100%;
+  height: 100%;
+}
+
+.company_info{
+  position: absolute;
+  top: 30%;
+  left: calc(50% - 300rpx);
+  width: 600rpx;
+  text-align: center;
+}
+.logo{
+  width: 185rpx;
+  height: 185rpx;
+}
+.desc{
+}
+
+.authorization_btn{
+  position: absolute;
+  top: 60%;
+  left: calc(50% - 345rpx);
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 690rpx;
+  padding: 0;
+  border: none;
+  border-radius: 20rpx;
+}
+.authorization_icon{
+  width: 60rpx;
+  height: 50rpx;
+}
+.authorization_desc{
+  margin-left: 30rpx;
+}
+
+.s-fc-1{
+  /* color: antiquewhite; */
+  color:#937d8a; 
+}
+.s-bg-1{
+  /* background: antiquewhite; */
+  background: #937d8a;
+}
+.s-fc-2{
+  color: #fff;
+}
+.s-bg-2{
+  background: #fff;
+}
+</style>
