23cd2e15abd08a406e42266ad912c6f9c1782955
元萌萌 <geekreading@163.com>
2018-11-23 19:05:49 +0800

    v1.0

--- config/prod.env.js
+++ config/prod.env.js
@@ -1,6 +1,5 @@
 module.exports = {
   NODE_ENV: '"production"',
   ENV_CONFIG: '"prod"',
-  // BASE_API: '"https://www.njjncm.com/"'
-  BASE_API: '"http://111.231.111.134:88/"'
+  BASE_API: '"https://www.njjncm.com/"'
 }
