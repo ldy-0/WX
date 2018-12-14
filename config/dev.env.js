87bf7140b4cb9aec8bac5ddaf5f4a46e51ad098c
xupeng <13294157862@163.com>
2018-12-14 14:33:40 +0800

    merge success 12-14

--- config/dev.env.js
+++ config/dev.env.js
@@ -1,9 +1,5 @@
 module.exports = {
   NODE_ENV: '"development"',
   ENV_CONFIG: '"dev"',
-<<<<<<< HEAD
   BASE_API: '"https://test.njjncm.com/"'
-=======
-  BASE_API: '"https://test.njjncm.com"'
->>>>>>> xp_dev_seller
 }
