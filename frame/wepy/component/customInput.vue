53ce3cb8795aa67eb0ee17ea6f0d56687434be84
person-0 <13213891450@163.com>
2019-04-17 20:47:53 +0800

    feat: wepy add customInput, customTextarea, formImg, formVideo component

---
+++ frame/wepy/component/customInput.vue
@@ -0,0 +1,98 @@
+<style scoped>
+.flex{
+  display: flex;
+  align-items: center;
+}
+.between{
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+}
+
+.input_wrap{
+  position: relative;
+  margin-bottom: 40rpx;
+}
+
+.title{
+  flex-shrink: 0;
+  margin: 0 30rpx 0 0;
+  font-size: 28rpx;
+}
+
+.input{
+  flex-grow: 1;
+  height: 100%;
+  padding-left: 20rpx;
+  border: 2rpx solid #c9c8d0;
+  border-radius: 4rpx;
+  font-size: 28rpx;
+}
+
+.required{
+  position: absolute;
+  top: calc(50% - 14rpx);
+  right: 20rpx;
+  font-size: 28rpx;
+}
+
+.s_fc_6{ color: #666; }
+.s_fc_10{ color: #ff0000; }
+</style>
+
+<template>
+
+  <view class='input_wrap flex' style='height: {{obj.height}}rpx;'>
+    <view class='title between' style="width: {{obj.width}}rpx;">
+      <repeat for="{{obj.title || obj.name}}">
+        <view>{{item}}</view>
+      </repeat>
+    </view>
+
+    <input class='input' value='{{obj.value}}' @input="input" />
+
+    <view class='required s_fc_10' wx:if="{{required}}">*</view>
+  </view>
+  
+</template>
+
+<script>
+import wepy from "wepy";
+
+export default class customInput extends wepy.component {
+  props = {
+    obj: Object
+  };
+
+  data = {
+    
+  };
+
+  computed = {
+    required(){ 
+      let required,
+          o = this.obj; 
+
+      required = 'alert' in this.obj;
+
+      if(required && !o.value) return o.alert = `请输入${o.title || o.name}`;
+
+      o.alert = null;
+      return required; 
+    }
+  }
+
+  methods = {
+    input(e){ 
+      let v = e.detail.value,
+          o = this.obj;
+
+      o.value = v;
+
+      // if(this.required && !v) return o.alert = `请输入${o.title || o.name}`;
+
+      // o.alert = null;
+    },
+  };
+}
+</script>
